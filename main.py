f7416a4722eec5257d48c4f1950a5375a1c08cf0
DeanLongmire <deanlongmire100@gmail.com>
2022-12-17 18:12:27 -0500

    Opens Webpage
    opens the vol football stat page for the 2022 season

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -9,5 +14,11 @@ opts.add_experimental_option("detach", True)
 driver = Chrome(chrome_options=opts)
 
 driver.get("https://www.sports-reference.com/cfb/schools/tennessee/2022.html")
+
+
+
+    
+
+
 
 
