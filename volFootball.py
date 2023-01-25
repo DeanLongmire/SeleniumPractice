442121aa1e0dd75b678cc70bf7239dd073a31669
jlongmi9 <jlongmi9@vols.utk.edu>
2023-01-25 15:27:03 -0500

    Made Game Save State
    Attempted to add a save code to the game

--- volFootball.py
+++ volFootball.py
@@ -15,6 +15,23 @@ driver = Chrome(chrome_options=opts)
 
 driver.get("https://www.sports-reference.com/cfb/schools/tennessee/2022.html")
 
+roster = driver.find_element(By.XPATH, '//*[@id="inner_nav"]/ul/li[4]/a')
+roster.click()
+
+time.sleep(5) #lets the page load
+
+roster = driver.find_element(By.TAG_NAME, "tbody")
+
+tableElements = roster.find_elements(By.TAG_NAME, "tr")
+
+time.sleep(5)
+
+for tableElement in tableElements:
+    rows = tableElement.find_elements(By.TAG_NAME, "td")
+    for row in rows:
+        print(row.text)
+
+
 
     
 
